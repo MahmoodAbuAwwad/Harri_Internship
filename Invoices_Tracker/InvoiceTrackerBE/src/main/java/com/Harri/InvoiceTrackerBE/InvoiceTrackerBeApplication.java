7a23da2c8b1c400f015f32afb3f3a2908422bbbb
Abu Hussien <abuhussien1996@gmail.com>
2021-06-30 10:26:13 +0300

    Adding FE and BE files

---
+++ Invoices_Tracker/InvoiceTrackerBE/src/main/java/com/Harri/InvoiceTrackerBE/InvoiceTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.Harri.InvoiceTrackerBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceTrackerBeApplication.class, args);
+	}
+
+}
---
+++ Invoices_Tracker/InvoiceTrackerBE/src/test/java/com/Harri/InvoiceTrackerBE/InvoiceTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Harri.InvoiceTrackerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
