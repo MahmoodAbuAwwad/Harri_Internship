0e8706ee3382a6a8c84508930cfcad20e5b2a0d4
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2021-07-01 10:18:19 +0300

    BE: signup request

--- Invoices_Tracker/InvoiceTrackerBE/src/main/java/com/Harri/InvoiceTrackerBE/services/UserService.java
+++ Invoices_Tracker/InvoiceTrackerBE/src/main/java/com/Harri/InvoiceTrackerBE/services/UserService.java
@@ -8,12 +8,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.constraints.Null;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepo;
     public ResponseEntity signUp( User user){
-        userRepo.save(user);
-        return new ResponseEntity<>("User Signed Up Successfully", HttpStatus.OK);
+        if(validateEmail(user.getEmail())) {
+            userRepo.save(user);
+            return new ResponseEntity<>("User Signed Up Successfully", HttpStatus.OK);
+        } else
+        {
+            return new ResponseEntity<>("email already exists", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private boolean validateEmail(String email){
+        if(userRepo.existsByEmail(email)){
+            //email exists in the db
+            return  false;
+        }
+        return true;
     }
 }
